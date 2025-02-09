269162dcede01b529585d26b697404876b5e0782
keitaMax <keita.nihei.1996.05.29@gmail.com>
2025-02-09 16:36:45 +0900

    fix time

--- led/led17-18.py
+++ led/led17-18.py
@@ -10,18 +10,30 @@ GPIO.setmode(GPIO.BCM)
 # GPIO17/18を出力モード設定
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+
+sleepTime = 0.1
 
 while True:
   # GPIO17の出力を1にして、LED点灯
   GPIO.output(17, 1)
   GPIO.output(18, 0)
+  GPIO.output(25, 0)
 
   # 0.5秒待つ
-  time.sleep(0.5)
+  time.sleep(sleepTime)
 
   # GPIO17の出力を0にして、LED消灯
   GPIO.output(17, 0)
   GPIO.output(18, 1)
+  GPIO.output(25, 0)
 
   # 0.5秒待つ
-  time.sleep(0.5)
+  time.sleep(sleepTime)
+  # GPIO17の出力を0にして、LED消灯
+  GPIO.output(17, 0)
+  GPIO.output(18, 0)
+  GPIO.output(25, 1)
+
+  # 0.5秒待つ
+  time.sleep(sleepTime)
